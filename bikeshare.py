56d92bd11fce1e072e6d5f32f281c5e970104e5a
nd31pr0 <ndzinfor@gmail.com>
2021-01-25 22:31:59 +0100

    feat: Commit done after updates to time stats

--- bikeshare.py
+++ bikeshare.py
@@ -84,7 +84,7 @@ def load_data(city, month, day):
 
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
+    """Displays statistics on the most frequent times of travel. Code refactoring"""
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
